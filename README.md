cfe5cba7e1a8202529fd25116f196c662e9c7fb0
Ed Outhwaite <ed.outhwaite@marklogic.com>
2019-04-30 08:10:56 +0100

    parameterised input, output, time and modifier

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # fix-subtitles
+
 This is a Perl script to fix srt subtitles
 
 Basically, I found an srt format subtitles file which were taken from a DVD rip of a TV show - dialogue in the version of the show on YouTube started 12 seconds earlier, so this script currently takes 12 seconds off.
 
-To Do:
-Add parameters for input file, output file and number of seconds...
+USAGE:
+
+    fix-srt-subtitles.pl source destination modifier seconds
+
+eg:
+
+    perl fix-srt-subtitles.pl LaPiovraS01E01_eng.srt LaPiovraS01E01.srt - 12
+
+source and destination should be input and output srt subtitles files
+modifier should be either + or -
+seconds should be the number of seconds to add or subtract
--- fix-srt-subtitles.pl
+++ fix-srt-subtitles.pl
@@ -1,34 +1,65 @@
-#/usr/bin/perl
+#!/usr/bin/perl
 use strict;
 use warnings;
+use Carp;
 use Time::Piece;
+
+sub addtime {
+  my $time = shift;
+  my $seconds = shift;
+  return $time += $seconds;
+}
+
+sub reducetime {
+  my $time = shift;
+  my $seconds = shift;
+  return $time -= $seconds;
+}
 
 sub fixtime {
   my $time = shift;
+  my $direction = shift;
+  my $seconds = shift;
+  my $newtime;
   print "$time -> ";
   my $t1 = Time::Piece->strptime( $time, '%H:%M:%S' );
-  # subtract 12 seconds
-  $t1 -= 12;
-  print $t1->hms;
+  if ($direction eq "+") { $newtime = addtime($t1, $seconds); }
+  else { $newtime = reducetime($t1, $seconds); }
+  print $newtime->hms;
   print "\n";
-  return $t1->hms;
+  return $newtime->hms;
 }
 
-open (INFILE, "LaPiovraS01E01_eng.srt");
-open (OUTFILE, ">LaPiovraS01E01.srt");
-while (my $line = <INFILE>) {
+my $usage = "USAGE:
+$0 source destination modifier seconds
+
+source and destination should be input and output srt subtitles files
+modifier should be either + or -
+seconds should be the number of seconds to add or subtract
+";
+if (@ARGV != 4) { croak $usage; }
+
+my $sourceFilename = $ARGV[0];
+my $outputFilename = $ARGV[1];
+my $fixDirection = $ARGV[2];
+my $fixSeconds = $ARGV[3];
+
+open (my $sourcefh, "<", $sourceFilename)
+  or die "Can't open < '$sourceFilename': $!";
+open (my $outputfh, ">", $outputFilename)
+  or die "Can't open > '$outputFilename': $!";
+while (my $line = <$sourcefh>) {
   chomp($line);
   if ($line =~ /^(\d{2}:\d{2}:\d{2})(,\d{3} --> )(\d{2}:\d{2}:\d{2})(,\d{3})/) {
     my $first = $1;
     my $mid = $2;
     my $second = $3;
     my $end = $4;
-    my $newfirst = fixtime($first);
-    my $newsecond = fixtime($second);
-    print OUTFILE "$newfirst$mid$newsecond$end\n";
+    my $newfirst = fixtime($first, $fixDirection, $fixSeconds);
+    my $newsecond = fixtime($second, $fixDirection, $fixSeconds);
+    print $outputfh "$newfirst$mid$newsecond$end\n";
   }
   else {
-    print OUTFILE "$line\n";
+    print $outputfh "$line\n";
   }
 }
-
